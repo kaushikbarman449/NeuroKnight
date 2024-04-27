f11c67f5e223d1e2597e585811b4c94a739bd8af
kaushikbarman449 <kaushikbarman449@gmail.com>
2024-04-27 12:55:20 +0530

    changed website title onClick link

--- app/(components)/Navbar.tsx
+++ app/(components)/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <div className={'flex justify-between w-full md:px-40 p-4 sticky h-16 inset-x-0 top-0 z-30 border-b-2 border-gray-200 bg-white/75 backdrop-blur-lg transition-all bg-white'}>
       <div className='flex items-center'>
-        <Link href='/Hero' className='text-2xl mt-2'>NeuroKnightMRI</Link>
+        <Link href='/' className='text-2xl mt-2'>NeuroKnightMRI</Link>
       </div>
       <div className='flex items-center'>
         <a href="https://github.com/kaushikbarman449" target='_blank'>
